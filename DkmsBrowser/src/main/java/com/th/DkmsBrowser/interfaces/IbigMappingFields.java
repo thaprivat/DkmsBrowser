b922323017caf9eff29486fa026b03e148fc6759
hanni12 <73305737+hanni12@users.noreply.github.com>
2021-11-30 18:14:08 +0100

    ver1.0

--- DkmsBrowser/src/main/java/com/th/DkmsBrowser/interfaces/IbigMappingFields.java
+++
@@ -1,7 +0,0 @@
-package com.th.DkmsBrowser.interfaces;
-
-import com.th.DkmsBrowser.dataObjects.BigObject;
-
-public interface IbigMappingFields {
-	public BigObject bigMappingFields(BigObject bigWorkFields);
-}
