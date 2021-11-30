835aa216ee696350b913f97991342b73e1f79d1e
hanni12 <73305737+hanni12@users.noreply.github.com>
2021-11-30 17:47:07 +0100

    ver4

--- src/main/java/com/th/DkmsBrowser/dataObjects/KdtObject.java
+++
@@ -1,9 +0,0 @@
-package com.th.DkmsBrowser.dataObjects;
-
-public class KdtObject {
-	public String Kno;
-	public String Ver;
-	public String Mtext;
-	public String InfoText;
-	public String Kgroups;
-}
--- src/main/java/com/th/DkmsBrowser/dataObjects/MappedFields.java
+++
@@ -1,9 +0,0 @@
-package com.th.DkmsBrowser.dataObjects;
-
-public class MappedFields {
-	public String kmgKeyLabel1ToText;
-	public String bigKmgKeyLabel3ToText;
-	public String bigKmgKeyLabel1ToText2;
-	public String kmgKnoToText;
-	public String yearlyOrOther;
-}
